462dacf5c07d8c3c1bfe13b7eea9c123942ed674
Calie Rushton <calielrushton@gmail.com>
2019-01-16 12:36:06 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
     # only break the loop if user types "exit"
       if input == number
         puts "You guessed the correct number!"
-      elsif (1..6).include?(input.to_i) && input != number
+      elsif (1..6).include?(input.to_i) && input != number # check its a number between 1-6
         puts "The computer guessed #{number}."
       elsif input == "exit"
         puts "Goodbye!"
